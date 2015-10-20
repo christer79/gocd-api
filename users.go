30ed565785e0a24e35d146efeeadaff390b8c661
Christer Eriksson <christer.j.eriksson@gmail.com>
2015-10-20 08:28:08 +0200

    Users modified to handle request of single user

--- users.go
+++ users.go
@@ -11,23 +11,41 @@ func (s *UserService) GetAll() ([]User, *Response, error) {
 
 	req, err := s.client.NewRequest("GET", u, nil)
 	if err != nil {
-		fmt.Println("ERROR CREATOING REQUEST")
+		fmt.Println("ERROR CREATING REQUEST")
 		return nil, nil, err
-
 	}
 
-	users := new([]User)
+	req.Header.Set("Accept", "application/vnd.go.cd.v1+json")
+
+	users := new(UserList)
 	response, err := s.client.Do(req, users)
 	if err != nil {
 		fmt.Println("ERROR EXECUTING REQUEST")
-		var body []byte
-		response.Body.Read(body)
-		fmt.Println(body)
 		return nil, response, err
 	}
-	var body []byte
-	response.Body.Read(body)
-	fmt.Println(body)
-	return *users, response, err
+
+	return users.UEmbedded.Users, response, err
+
+}
+
+func (s *UserService) Get(loginName string) (*User, *Response, error) {
+	u := "api/users/" + loginName
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		fmt.Println("ERROR CREATING REQUEST")
+		return nil, nil, err
+	}
+
+	req.Header.Set("Accept", "application/vnd.go.cd.v1+json")
+
+	user := new(User)
+	response, err := s.client.Do(req, user)
+	if err != nil {
+		fmt.Println("ERROR EXECUTING REQUEST")
+		return nil, response, err
+	}
+
+	return user, response, err
 
 }
