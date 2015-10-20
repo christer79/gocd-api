b648b2f4f79a1b919d18d51d3297218753d9e38f
Christer Eriksson <christer.j.eriksson@gmail.com>
2015-10-20 22:09:28 +0200

    Adde ldap to docker test setup

--- gocd-docker/launch.sh
+++ gocd-docker/launch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-docker run --rm -d --name gocd-server -p 8153:8153 gocd/gocd-server
+docker run -d  --name openldap -e LDAP_ORGANISATION="Test" -e LDAP_DOMAIN=localhost.localdomain -e LDAP_ADMIN_PASSWORD=JonSn0w osixia/openldap 
 
-docker run --rm -d --link gocd-server:go-server  gocd/gocd-agent
-docker run --rm -d --link gocd-server:go-server  gocd/gocd-agent
+docker run -d  --link openldap:ldap --name gocd-server -p 8153:8153 gocd/gocd-server
+
+docker run -d  --link gocd-server:go-server  gocd/gocd-agent
+docker run -d  --link gocd-server:go-server  gocd/gocd-agent
+
+
+docker run -d  -p 443:443 --link openldap:ldap -e PHPLDAPADMIN_LDAP_HOSTS=ldap osixia/phpldapadmin
